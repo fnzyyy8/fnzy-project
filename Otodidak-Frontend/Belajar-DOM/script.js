559aace7e4633a0d198278b162c82fcbc98dcdc1
Fnzy888 <farhanshidayat@gmail.com>
2023-07-08 10:46:23 +0700

    Penggabungan seluruh belajar dan penggunaan DOM

---
+++ Otodidak-Frontend/Belajar-DOM/script.js
@@ -0,0 +1,7 @@
+// ! document.getElementById() -> element
+const judul = document.getElementById("judul");
+
+// document.getElementsByTagName() -> HTML COllections
+
+const p = document.getElementsByTagName("p");
+p[1].style.backgroundColor = "Yellow";
