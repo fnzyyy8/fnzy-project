c88f4646add8ac75d6fb97571e0fa01cc0c3d889
Fnzy888 <farhanshidayat@gmail.com>
2023-07-08 11:10:15 +0700

    Belajar query selector

--- Otodidak-Frontend/Belajar-DOM/script.js
+++ Otodidak-Frontend/Belajar-DOM/script.js
@@ -1,7 +1,13 @@
 // ! document.getElementById() -> element
 const judul = document.getElementById("judul");
+judul.innerHTML = "Hallo Farhan";
 
-// document.getElementsByTagName() -> HTML COllections
-
+// ! document.getElementsByTagName() -> HTML COllections
 const p = document.getElementsByTagName("p");
-p[1].style.backgroundColor = "Yellow";
+p[1].style.backgroundColor = "pink";
+
+// ! document.querySelector() -> element
+const li2 = document.querySelector("section#b ul li:nth-child(2) ");
+li2.style.backgroundColor = "pink";
+
+// ! document.querySelectorAll() -> Node list
